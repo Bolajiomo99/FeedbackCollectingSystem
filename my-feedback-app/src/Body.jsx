59b9354868886086d6ab11d01c197697bd07aff2
Bolaji Jimoh Adigun <bolajijimoh8@gmail.com>
2024-05-07 16:08:44 +0100

    #

--- my-feedback-app/src/Body.jsx
+++ my-feedback-app/src/Body.jsx
@@ -5,7 +5,7 @@ function Body(){
     return(
         <div>
             <strong><h2>Share your Feedback</h2></strong>
-            <p>Rate your Experience</p>
+            <p>Rate your Experiences</p>
             <p>Terrible   Poor   Average   Good   Awesome</p>
             <strong>Comment</strong>(Optional)<br/>
             <button>Enter your Feedback</button>
